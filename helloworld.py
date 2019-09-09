b604460dbfb629091c2e05743561a6638ab24084
Ryan Siu <rsiu05@gmail.com>
2019-09-09 13:13:23 -0400

    Finish lab 0

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+import random
+
+print ("Ryan Siu")
+a = random.randint(0, 100)
+b = random.randint(0, 100)
+print (a)
+print (b)
+print ("Sum = " + str(a + b))
+print ("Average = " + str((a + b) / 2))
